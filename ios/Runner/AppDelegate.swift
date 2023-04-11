524e4b95ddec9929391c9b61146b8f8695f70c1e
Suman Adhikari <expertxkid@gmail.com>
2023-04-11 11:13:48 +0545

    refactor/FKDA-1: setup Maps API key for iOS and android

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+  GMSServices.provideAPIKey("AIzaSyDAIY89n4Q5Mzf72sXW1B7voqWsvOP-oD0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
